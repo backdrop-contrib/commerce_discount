dc8dbb391fed48636fb182b8d68b44ab38a940df
nvahalik <nvahalik@21689.no-reply.drupal.org>
2015-11-24 14:05:01 -0800

    Issue #2600044 by nvahalik, mglaman, Neograph734: Merge Discount Date and Usage into Commerce Discount

--- js/commerce_discount.js
+++ js/commerce_discount.js
@@ -62,5 +62,47 @@
         return Drupal.t('Incompatible with all');
       }
     });
+    $('fieldset#edit-commerce-discount-fields-additional-settings-discount-date', context).drupalSetSummary(function(context) {
+      var fromDate,
+          toDate,
+          fromDateTS,
+          toDateTS;
+
+      fromDate = $('input[name="commerce_discount_fields[commerce_discount_date][und][0][value][date]"]').val();
+      fromDateTS = fromDate ? new Date(fromDate) : 0;
+      toDate = $('input[name="commerce_discount_fields[commerce_discount_date][und][0][value2][date]"]').val();
+      toDateTS = toDate ? new Date(toDate) : 0;
+
+      options = {
+        '!from': fromDateTS < Date.now() ? Drupal.t('Started') : Drupal.t('Starts'),
+        '!to': toDateTS < Date.now() ? Drupal.t('Ended') : Drupal.t('Ends'),
+        '@fromDate': fromDate,
+        '@toDate': toDate
+      };
+
+      if (fromDate && toDate) {
+        options['!to'] = options['!to'].toLowerCase();
+        return Drupal.t('!from @fromDate and !to @toDate', options);
+      }
+      else if (fromDate && !toDate) {
+        return Drupal.t('!from @fromDate', options);
+      }
+      else if (!fromDate && toDate) {
+        return Drupal.t('!to @toDate', options);
+      }
+      else {
+        return Drupal.t('Always active');
+      }
+    });
+    $('fieldset#edit-commerce-discount-fields-additional-settings-commerce-discount-usage', context).drupalSetSummary(function(context) {
+      var usagePerPerson = $('input[name="commerce_discount_fields[discount_usage_per_person][und][0][value]"]').val(),
+          overallUsage = $('input[name="commerce_discount_fields[discount_usage_limit][und][0][value]"]').val(),
+          vals = [];
+
+      vals.push(Drupal.t('!usagePerPerson per person', {'!usagePerPerson': usagePerPerson > 0 ? Drupal.checkPlain(usagePerPerson) : '&#8734;'}));
+      vals.push(Drupal.t('!overallUsage total', {'!overallUsage': overallUsage > 0 ? Drupal.checkPlain(overallUsage): '&#8734;'}));
+
+      return vals.join(', ');
+    });
   };
 }(jQuery, Drupal));
